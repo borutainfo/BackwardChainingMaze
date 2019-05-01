c4b853ae8634cccc0fed1fe3024cb9454c768d8b
Boruta <sebastian@boruta.info>
2019-05-01 19:54:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+BackwardChaining
+---
+`com.boruta.backwardchaining`
+
+Project for studies in the field of "Intelligent computing techniques".
---
+++ src/main/java/com/boruta/backwardchaining/App.java
@@ -0,0 +1,8 @@
+package com.boruta.backwardchaining;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("gites");
+    }
+}
