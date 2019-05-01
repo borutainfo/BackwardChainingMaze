b3d32213ef4b3a55e47c9caf453543be0899dbde
Boruta <sebastian@boruta.info>
2019-05-01 20:06:39 +0200

    drools

--- src/main/java/com/boruta/backwardchaining/App.java
+++ src/main/java/com/boruta/backwardchaining/App.java
@@ -3,6 +3,7 @@ package com.boruta.backwardchaining;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("gites");
+
+        System.out.println("gite2s");
     }
 }
